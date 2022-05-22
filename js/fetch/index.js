f71e353a5aba34529d9d0d571deeb2236f315420
Jahangir Shah <shah@api4.ai>
2022-05-23 00:07:47 +0300

    Added JS/fetch sample.

---
+++ js/fetch/index.js
@@ -0,0 +1,80 @@
+// Example of using API4AI masks detection.
+
+// Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
+// For more details visit:
+//   https://api4.ai
+
+// Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
+// For more details visit:
+//   https://rapidapi.com/api4ai-api4ai-default/api/masks-detection/details
+const MODE = 'demo'
+
+// Your RapidAPI key. Fill this variable with the proper value if you want
+// to try api4ai via RapidAPI marketplace.
+const RAPIDAPI_KEY = ''
+
+const OPTIONS = {
+  demo: {
+    url: 'https://demo.api4ai.cloud/med-mask/v1/results',
+    headers: { 'A4A-CLIENT-APP-ID': 'sample' }
+  },
+  rapidapi: {
+    url: 'https://masks-detection.p.rapidapi.com/v1/results',
+    headers: { 'X-RapidAPI-Key': RAPIDAPI_KEY }
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  const input = document.getElementById('file')
+  const raw = document.getElementById('raw')
+  const sectionRaw = document.getElementById('sectionRaw')
+  const parsed = document.getElementById('parsed')
+  const sectionParsed = document.getElementById('sectionParsed')
+  const spinner = document.getElementById('spinner')
+
+  input.addEventListener('change', (event) => {
+    const file = event.target.files[0]
+    if (!file) {
+      return false
+    }
+
+    sectionRaw.hidden = true
+    sectionParsed.hidden = true
+    spinner.hidden = false
+
+    // Preapare request.
+    const form = new FormData()
+    form.append('image', file)
+    const requestOptions = {
+      method: 'POST',
+      body: form,
+      headers: OPTIONS[MODE].headers
+    }
+
+    // Make request.
+    fetch(OPTIONS[MODE].url, requestOptions)
+      .then(response => response.json())
+      .then(function (response) {
+        // Print raw response.
+        raw.textContent = JSON.stringify(response, undefined, 2)
+        sectionRaw.hidden = false
+        // Get mask data from objects.
+        const maskData = response.results[0].entities[0].objects
+        const withMaskCount = maskData
+          .filter((obj) => obj.entities
+            .some((ent) => ent.name === 'med-mask' && ent.classes.mask > ent.classes.nomask))
+          .length
+        parsed.textContent = `Total people found: ${maskData.length}\n`
+        parsed.textContent += `With mask: ${withMaskCount}\n`
+        parsed.textContent += `Without mask: ${maskData.length - withMaskCount}`
+        sectionParsed.hidden = false
+      })
+      .catch(function (error) {
+        // Error can be handled here.
+        console.error(error)
+      })
+      .then(function () {
+        spinner.hidden = true
+      })
+  })
+})
